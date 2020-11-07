afeb3f6bbca50a7e8234afb5ab4d09fd088eb5ae
Saygın YILDIZ <45289808+rowers7@users.noreply.github.com>
2020-11-07 20:57:20 +0300

    Add files via upload

---
+++ deneme/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <Navbar/>
+  </div>
+  
+</template>
+<script>
+  import globalComponent from "./components/globalComponent";
+export default {
+  components:{
+    ...globalComponent
+  }
+}
+
+
+
+</script>
+<style>
+
+ .deneme{
+    background-color:blue!important;
+ }
+
+  .tumkategoriler{
+    background-color:gray!important;
+    margin-left: 200px;
+    width: 200px;
+ }
+
+.navbar-brand {
+  margin-left: 70px;
+  font-size: 42px!important;
+}
+.navbar-brand2 {
+  margin-left: 90px;
+  font-size: 13px;
+}
+
+.üst_bar {
+  background-color:blue!important;
+  color:white;
+}
+
+.deee {
+  margin-left: 70px;
+}
+.nokta {
+  font-size: 30px;
+}
+
+.uzak_mesafe {
+  margin-left: 520px;
+}
+.mesafe {
+  margin-left: 30px;
+}
+
+
+  .arama{
+    background-color:yellow!important;
+    
+ }
+
+  .arama2{
+    background-color:white!important;
+    width: 400px!important;
+ }
+
+
+  .arama3{
+    background-color:transparent!important;
+    color: black;
+    
+ }
+.arama3:hover {
+  color: red;
+}
+
+.beyaz {
+  color: white;
+}
+
+</style>
---
+++ deneme/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+
+
+
+<div>
+<div class="üst_bar">
+  <div class="navbar-brand2">
+    <b-navbar-brand2 href="#" >0542 532 35 90</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="deee">info@ebrarbilgisayar.com</b-navbar-brand2>
+    
+    <b-navbar-brand2 href="#" class="uzak_mesafe"></b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="nokta">.</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="dee">Toplama Pc</b-navbar-brand2>
+    
+    <b-navbar-brand2 href="#" class="mesafe"></b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="nokta">.</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="dee">Sipariş Takibi</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="mesafe"></b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="nokta">.</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="dee">Kıyaslama Listesi</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="mesafe"></b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="nokta">.</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="dee">Yardım</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="mesafe"></b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="nokta">.</b-navbar-brand2>
+    <b-navbar-brand2 href="#" class="dee">İletişim</b-navbar-brand2>
+    
+
+    
+  </div>
+  
+</div>
+
+  <b-navbar toggleable="lg" type="dark" variant="info" class="deneme">
+    
+    <b-navbar-brand href="#" >ebrarbilgisayar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        
+        
+      </b-navbar-nav>
+
+<b-navbar-nav class="deneme" >
+
+<b-nav-item-dropdown text="Tüm Kategoriler" class="tumkategoriler">
+          <b-dropdown-item href="#">Masaüstü</b-dropdown-item>
+          <b-dropdown-item href="#">Dizüstü</b-dropdown-item>
+          <b-dropdown-item href="#">Bilgisayar Bileşenleri</b-dropdown-item>
+          <b-dropdown-item href="#">Diğer</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+
+      <!-- Right aligned nav items -->
+      
+        <b-nav-form class="arama">
+          <b-form-input size="md" class="arama2" placeholder="Ürün arayın..."></b-form-input>
+          <b-button size="sm" class="arama3" type="submit">ARA</b-button>
+        </b-nav-form>
+
+        
+
+        
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+
+</template>
+    
